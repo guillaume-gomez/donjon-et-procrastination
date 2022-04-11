2182c3a68dbc35b136d93c12abf60b7e292354c1
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-04-11 18:07:23 +0200

    :construction: start to implement the logo and the background

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import logo from "./LOGO_1.png";
+import subtitle from "./LOGO_2.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-      <main>
+      <main className="body-container">
+          <img src={logo} />
+          <img src={subtitle} />
       </main>
     </div>
   );
