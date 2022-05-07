1ba89987a77e12e3d8f2c94df9fb6209c22651bb
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-05-07 15:01:34 +0200

    add animations

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import logo from "./images/LOGO_1.png";
 import subtitle from "./images/LOGO_2.png";
 import Generator from "./components/Generator";
+import { motion } from "framer-motion";
 
 function App() {
   return (
@@ -10,8 +11,22 @@ function App() {
       <header className="App-header">
       </header>
       <main className="body-container">
-          <img src={logo} />
-          <img src={subtitle} />
+          <div className="logo-container">
+            <motion.div
+              initial={{ y: -100, zIndex:2 }}
+              animate={{ y: 55 }}
+              transition={{ type: "spring", bounce: 0.35, duration: 2 }}
+            >
+              <img src={logo} />
+              </motion.div>
+            <motion.div
+              initial={{ opacity: 0, zIndex: 1 }}
+              animate={{ opacity: 1 }}
+              transition={{  duration: 1 }}
+            >
+            < img src={subtitle} />
+            </motion.div>
+          </div>
           <Generator />
       </main>
     </div>
