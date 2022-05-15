9e437fa80bb03b276d9bc0a028c85c985d04ccee
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-05-15 16:53:55 +0200

    passing position to word

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -40,7 +40,7 @@ function Generator() {
   return (
     <div id="generator-container">
       <div id="words-container" className="">
-        {words.map((word, index) => <RandomWord key={index} word={word} />)}
+        {words.map((word, index) => <RandomWord key={index} word={word} position={index} />)}
       </div>
       <Button onClick={generate} />
     </div>
