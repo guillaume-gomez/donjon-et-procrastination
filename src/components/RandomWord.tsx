c2cc705944efdf70710d93b68dcba093819330cd
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-05-16 00:05:26 +0200

    add space when world is splitted

--- src/components/RandomWord.tsx
+++ src/components/RandomWord.tsx
@@ -4,24 +4,15 @@ import "./RandomWord.css"
 
 interface RandomWordProps {
   word: string;
-  position: number
 }
 
-/*function fromWordToThinking(word: string) {
-  const array = word.split("");
-  return array
-    .map(() => ".")
-    .join('')
-    .toString();
-}*/
-
-function RandomWord({ word, position } : RandomWordProps) {
+function RandomWord({ word } : RandomWordProps) {
   const controls = useAnimation();
   useEffect(() => {
     controls.start('roll').then(() => {
       controls.set({rotateX: 0})
     })
-  }, [word, controls])
+  }, [controls])
 
   return (
     <motion.div id="random-word-container">
@@ -32,11 +23,12 @@ function RandomWord({ word, position } : RandomWordProps) {
                 animate={controls}
                 initial="initial"
                 transition={
-                  { delay: position * 0.25 * (index *0.1), duration: 1, ease: 'easeOut' }
+                  { delay: 0.25 * (index *0.1), duration: 1, ease: 'easeOut' }
                 }
                 variants={{
                   roll: { rotateX: 360 * 5  }
                 }}
+                style={{padding: letter === " " ? "0 0.5rem" : 0 }}
               >
               {letter}
               </motion.span>
