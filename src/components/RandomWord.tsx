26cf77d68c7d87e9101cc35012fa12846150cbc9
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-05-15 16:54:26 +0200

    add animation when the word changed

--- src/components/RandomWord.tsx
+++ src/components/RandomWord.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { motion, useAnimation } from "framer-motion";
 import "./RandomWord.css"
 
 interface RandomWordProps {
   word: string;
+  position: number
 }
 
-function RandomWord({word} : RandomWordProps) {
+function fromWordToThinking(word: string) {
+  const array = word.split("");
+  return array
+    .map(() => ".")
+    .join('')
+    .toString();
+}
+
+function RandomWord({ word, position } : RandomWordProps) {
+  const controls = useAnimation();
+  useEffect(() => {
+    controls.start('roll').then(() => {
+      controls.set({rotateX: 0})
+    })
+  }, [word])
+
   return (
-    <div id="random-word-container">
-      {word}
-    </div>
+    <motion.div id="random-word-container"
+          animate={controls}
+          initial="initial"
+          transition={
+            { delay: position * 0.25, duration: 1, ease: 'easeOut' }
+          }
+          variants={{
+            roll: { rotateX: 360 * 5  }
+          }}
+          >
+        {word}
+    </motion.div>
   );
 }
 
